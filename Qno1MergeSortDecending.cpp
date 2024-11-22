65d056c7351330dae2ec5e66ecb908aa1ec05c79
Safeer Ahmad Mir <123501686+Safeermir@users.noreply.github.com>
2024-11-23 02:34:19 +0530

    Add files via upload

---
+++ Qno1MergeSortDecending.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+void merge(vector<int> &a,vector<int> &b,vector<int> &res){
+    int i=0;
+    int j=0;
+    int k=0;
+    while(i<a.size() && j<b.size()){
+         if(a[i]>=b[j]) res[k++]=a[i++];
+         else res[k++]=b[j++];
+    }
+    if(i==a.size()) while(j<b.size()) res[k++]=b[j++];//a is at end
+    if(j==b.size()) while(i<a.size()) res[k++]=a[i++];//b is at end
+}
+void mergeSortDe(vector<int>&v){
+    int size=v.size();
+    if(size==1) return;
+    int n1=size/2,n2=size-n1;
+    vector<int> v1(n1),v2(n2);
+    for(int i=0;i<n1;i++) v1[i]=v[i];
+    for(int i=0;i<n2;i++) v2[i]=v[i+n1];
+    mergeSortDe(v1);
+    mergeSortDe(v2);
+    merge(v1,v2,v);
+}
+int main(){
+    int arr[]={1,2,3,4,5,6,7,8,9};
+    int n=sizeof(arr)/sizeof(arr[0]);
+    vector<int> v(arr,arr+n);
+    for(int i=0;i<v.size();i++){
+        cout<<v[i]<<" ";
+    }
+    cout<<endl;
+    mergeSortDe(v);
+    for(int i=0;i<v.size();i++){
+        cout<<v[i]<<" ";
+    }
+
+    
+}
---
+++ Qno2ReversePairCount.cpp
@@ -0,0 +1,49 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int ReversePairs(vector<int> &a,vector<int> &b){
+    int i=0,j=0;
+    int count=0;
+    while(i<a.size() && j<b.size()){
+        if((long long )a[i]>(long long) 2*b[j]) {  //Calculate the number of reverse pairs
+            count+=(a.size()-i);
+            j++;
+        }
+        else{ 
+            i++;
+        }
+    }
+    return count;
+}
+void merge(vector<int> &a,vector<int> &b,vector<int> &res){
+    int i=0,j=0,k=0;
+    while(i<a.size() && j<b.size()){
+        if(a[i]<=b[j]) res[k++]=a[i++];
+        else res[k++]=b[j++];
+    }
+    if(i==a.size()) while(j<b.size()) res[k++]=b[j++];//a is at end
+    if(j==b.size()) while(i<a.size()) res[k++]=a[i++];//b is at end
+}
+int mergeSort(vector<int>& v){
+    int count=0;
+    int size=v.size();
+    if(size==1) return 0;
+    int n1=size/2,n2=size-size/2;
+    vector<int> v1(n1),v2(n2);
+    for(int i=0;i<n1;i++) v1[i]=v[i];// Copy paste
+    for(int i=0;i<n2;i++) v2[i]=v[i+n1];
+    count+=mergeSort(v1);
+    count+=mergeSort(v2);
+    count+=ReversePairs(v1,v2);
+    merge(v1,v2,v);
+    v1.clear();
+    v2.clear();
+    return count;
+}
+int main(){
+    int arr[]={1,3,2,3,1};
+    int n=sizeof(arr)/sizeof(arr[0]);
+    vector<int> v(arr,arr+n);
+    cout<<endl;
+    cout<<"Number of reverse Pair is: "<<mergeSort(v);
+}
